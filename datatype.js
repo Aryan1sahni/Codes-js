df624f7f4ccc131db9d0a467bc0e9e6200290dce
Aryan1sahni <aryan1sahni@gmail.com>
2024-08-17 21:32:13 +0530

    datatypes in js

---
+++ datatype.js
@@ -0,0 +1,9 @@
+
+
+let name ="Joker"       // string(we can use single quotes as well as double quotes)
+let age = 12            //number
+let isLogged = false    //boolean
+let account = null      // standalone value (means nothing or empty)
+
+console.log(typeof(age))
+console.log(typeof(account))
