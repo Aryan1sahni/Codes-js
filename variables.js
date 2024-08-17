2382cfad2d34ff1965b45e316428eca723199454
Aryan1sahni <aryan1sahni@gmail.com>
2024-08-17 17:05:02 +0530

    Learning Variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553                // cannot be changed if declared with const
+let accountEmail ="example@gmail.com"
+var accountPassword ="1234"
+let accountCity = "Jaipur"
+let accountState;
+
+// accountId=123
+accountEmail= "hc@gmail.com"
+accountPassword="232"
+accountCity ="Bangalore"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
