ccabcc2f31172ac230b93f2b332c4d4d5e1704cc
Aryan1sahni <aryan1sahni@gmail.com>
2024-08-20 23:00:09 +0530

    more datatypes in js

---
+++ datatype2.js
@@ -0,0 +1,22 @@
+// Primitive 
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid)
+
+const bignumber = 35324234343434234n  // Big Int
+
+
+// Reference (Non primitive)
+
+const cars=["merc","bmw","audi","lambo"]
+{
+    name:"joker"
+    age:12
+    gender:"ma"
+}
+
+const myfun = function(){
+    console.log("this is function")
+}
