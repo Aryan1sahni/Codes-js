975ca1d2b8342005568616f89c8575b2c3f18bc3
Aryan1sahni <aryan1sahni@gmail.com>
2024-08-20 21:48:04 +0530

    conversion in js

---
+++ conversion.js
@@ -0,0 +1,30 @@
+    let score = "99"
+    let score2 = "12ab"
+
+    console.log("Hello")
+    console.log(typeof(score))
+
+    // "33" => 33
+    // "33abc" => NaN
+    //  true => 1; false => 0
+
+    let valueinNumber = Number(score)
+    let valueinNUmber2 = Number(score2)
+    console.log(typeof valueinNumber)
+    console.log(typeof valueinNUmber2)
+    console.log(valueinNUmber2)
+
+    let isloggedIN = 1
+
+    // 1 => true; 0 => false
+    // "" => false
+    // "hitesh" => true
+
+    let booleanisloggedIn = Boolean(isloggedIN)
+    console.log(booleanisloggedIn)
+
+    let someNumber =12
+    let StringNumber = String(someNumber)
+
+    console.log(StringNumber)
+    console.log(typeof StringNumber)
