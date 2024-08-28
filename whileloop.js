9192d2ca070d30fd1d8be8f64e27e388aee6454a
Aryan1sahni <aryan1sahni@gmail.com>
2024-08-29 00:06:36 +0530

    While and Do While loop is js

---
+++ whileloop.js
@@ -0,0 +1,19 @@
+let a =0
+while(a<10){
+    console.log("a is greater"+a)
+    a++
+}
+
+
+let myarr =["joker","batman","superman"]
+let i =0
+while(i<myarr.length){
+    console.log( myarr[i])
+    i++
+}
+
+let b=30
+do{
+    console.log("this will excute")
+    
+}while(b<20);
