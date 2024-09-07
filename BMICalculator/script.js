f7a4921d81995fc332f6151093f423d58c1267b9
Aryan1sahni <aryan1sahni@gmail.com>
2024-09-07 16:23:36 +0530

    BMi Calculator using html css and js

---
+++ BMICalculator/script.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("form")
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#Weight').value)
+    const results = document.querySelector('#results')
+
+    if(height === ' ' || height < 0 || isNaN(height) ){
+        results.innerHTML = "Please give a valid height"
+    }
+    else if(weight === ' ' || weight < 0 || isNaN(weight) ){
+        results.innerHTML = "Please give a valid weight"
+    }
+    else{
+        const bmi = (weight / ((height *height)/10000)).toFixed(2) 
+        if(bmi <18.6){
+        results.innerHTML = "BMI is "+bmi + " Under Weight"
+        }
+        else if(bmi >18.6 && bmi <24.9){
+            results.innerHTML = "BMI is "+bmi + " Normal Range"
+        }
+        else if(bmi >24.9){
+            results.innerHTML = "BMI is "+bmi + " Overweight"
+        }
+        
+    }
+})
