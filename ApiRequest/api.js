ee1f24cf5a6fe903e9b77d050e1df9377c75bc66
Aryan1sahni <aryan1sahni@gmail.com>
2024-10-26 20:23:52 +0530

    Api in js

---
+++ ApiRequest/api.js
@@ -0,0 +1,12 @@
+const xhr = new XMLHttpRequest()
+const requestUrl = 'https://api.github.com/users/aryan1sahni'
+xhr.open('GET',requestUrl)
+xhr.onreadystatechange = function(){
+    console.log(xhr.readyState)
+    if(xhr.readyState === 4){
+        // console.log(this.responseText)
+        const data = JSON.parse(this.responseText)  // parse converts string to a json
+        console.log(data.followers)
+    }
+}
+xhr.send()
